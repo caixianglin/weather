03c26b8785992f9ecfca4f80dd403058d5a88cf6
caixianglin <cxiangl1990@163.com>
2022-05-26 18:54:54 +0800

    remove chunk

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { IRouterConfig, lazy } from 'ice';
+import Detail from '@/pages/Detail';
+import Home from '@/pages/Home';
+import NotFound from '@/components/NotFound';
 
-const Home = lazy(() => import('@/pages/Home'));
-const Detail = lazy(() => import('@/pages/Detail'));
-const NotFound = lazy(() => import('@/components/NotFound'));
+// const Home = lazy(() => import('@/pages/Home'));
+// const Detail = lazy(() => import('@/pages/Detail'));
+// const NotFound = lazy(() => import('@/components/NotFound'));
 
 const routerConfig: IRouterConfig[] = [
   {
